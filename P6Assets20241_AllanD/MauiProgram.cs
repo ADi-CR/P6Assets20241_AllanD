8fa511ddaf7008646701cc1f3a8b047493a932a4
Allan Delgado C <allandelgadoc@gmail.com>
2024-03-21 21:06:59 -0600

    Semana9

--- P6Assets20241_AllanD/MauiProgram.cs
+++ P6Assets20241_AllanD/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace P6Assets20241_AllanD
 {
@@ -7,18 +8,14 @@ namespace P6Assets20241_AllanD
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-#if DEBUG
-    		builder.Logging.AddDebug();
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            }).UseMauiCommunityToolkit();
+#if DEBUG6
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
---
+++ P6Assets20241_AllanD/Services/WebAPIConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6Assets20241_AllanD.Services
+{
+    public static class WebAPIConnection
+    {
+        //acá definimos la ruta raíz del URL del servicio web (API Rest)
+        //a esta raíz, que llamaremos Prefijo, se le debe adjuntar 
+        //el resto de la ruta URL, que llamares Sufijo, para cada
+        //endpoint que vayamos a usar 
+
+        //además, es importante indicar que normalmente los APIs 
+        //trabajan en dos versiones: Pruebas y Producción 
+
+        public static string ProductionURLPrefix = "http://192.168.0.10:45457/api/";
+
+        public static string TestingURLPrefix = "http://192.168.0.10:45457/api/";
+
+        //además de la raíz de URL acá vamos a indicar cuál es la API Key que deberíamos 
+        //usar para consumir los endpoints 
+        public static string ApiKeyName = "P6ApiKey";
+        public static string ApiKeyValue = "AllanDP6Apik123454321abc";
+               
+
+    }
+}
